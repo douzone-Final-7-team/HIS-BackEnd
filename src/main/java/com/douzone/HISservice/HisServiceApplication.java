57043a68245a54470c719bfaec58c0b95d256eba
choko5901 <ehdgk5901@gmail.com>
2022-10-31 19:38:56 +0900

    start project

---
+++ src/main/java/com/douzone/HISservice/HisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.HISservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisServiceApplication.class, args);
+	}
+
+}
